611cb6019a40c2ee2398543e2bd715d3c2344cdf
manalidhokale <88763155+manalidhokale@users.noreply.github.com>
2021-08-18 11:52:40 +0530

    Add files via upload

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+
+	$servername = "localhost";
+	$username = "id17422226_root";
+	$password = "Manalidhokale@123";
+	$dbname = "id17422226_d_b";
+
+	$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+	if(!$conn){
+		die("Could not connect to the database due to the following error --> ".mysqli_connect_error());
+	}
+
+?>
---
+++ index.php
@@ -0,0 +1,73 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="stylesheet" type="text/css" href="css/navbar.css">
+
+    <title>Basic Banking System</title>
+  </head>
+
+  <body>
+  <?php
+  include 'navbar.php';
+  ?>
+  
+      
+      <!-- Introduction section -->
+            <div class="row intro py-1">
+              <div class="col-sm-12 col-md">
+                <div class="heading text-center my-5">
+                  <h3 class="text-center">Welcome to</h3>
+                  <h1>BANK OF INDIA</h1>
+                </div>
+              </div></div>
+      <div class container="fluid">
+      <div class="p-3 mb-2 bg-info text-white">
+      <div class="col-sm-14 col-md img text-center">
+                <img src="img/bank1.png" class="img-fluid pt-2" width="250px">
+      </div></div></div>
+
+
+
+      
+     <!-- Activity section -->
+           <div>
+             <div class="text-center">
+             <div class="row">
+                  <div class="col">
+                  <div class="col-md act">
+                    <img src="img/imgt.png" class="rounded float-start" width="150px">
+                    <br>
+                    <a href="transfermoney.php"><button style="background-color : #2785C4;">Make a Transaction</button></a>
+                  </div></div>
+                  <div class="col">
+                  <div class="text-center">
+                  <div class="col-md act">
+                    <img src="img/imgg.png" class="rounded float-start" width="150px">
+                    <br>
+                    <a href="transactionhistory.php"><button style="background-color : #2785C4;">Transaction History</button></a>
+                  </div>
+                  </div></div>
+              </div> 
+            </div>
+      </div>
+      <footer class="text-center mt-5 py-2">
+       
+      <div class="text-center">
+                    <p><span class="point fa fa-location-arrow"><b>Address :</b><br> </span>Bank of India, Near City-space, pune-410576</p>
+                    <p><span class="point glyphicon glyphicon-phone-alt"> Telephone: </span> 020-997 934 224</p>
+                    <p ><span class="glyphicon glyphicon-phone point"> Mobile: </span> +91-8564125793</p>
+                    <p ><span class="glyphicon glyphicon-envelope point"> Mail: </span> bankofindia@gmail.com</p>
+      </div><br>
+      <p>&copy 2021. Made by <b>MANALI DHOKALE</b> <br> M D Foundation</p>
+      </footer>
+      <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+  </body>
+</html>
